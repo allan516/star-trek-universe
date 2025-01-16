0e84cdb48b11a0d9ec53bb9f980c74221b13731a
Allan <allan.mendes0976@gmail.com>
2025-01-15 23:10:24 -0300

    feat: create percentage calculation on scroll

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -52,22 +52,25 @@ export class HomeComponent {
   @HostListener('window:scroll', ['$event'])
   onScroll(event: Event): void {
     const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    const documentHeight =
+      document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercentage = (scrollPosition / documentHeight) * 100;
 
-    if (scrollPosition > 10) {
+    if (scrollPercentage > 10) {
       // Defina a posição do scroll que irá acionar a animação
       this.scrollState = 'scrolled'; // Ativa o estado "scrolled" quando o scroll passa de 10px
     } else {
       this.scrollState = 'notScrolled'; // Retorna ao estado "notScrolled"
     }
 
-    if (scrollPosition > 930) {
+    if (scrollPercentage > 50) {
       // Defina a posição do scroll que irá acionar a animação
       this.shipScrollState = 'shipScrolled'; // Ativa o estado "scrolled" quando o scroll passa de 1000px
     } else {
       this.shipScrollState = 'notShipScrolled'; // Retorna ao estado "notScrolled"
     }
 
-    if (scrollPosition > 940) {
+    if (scrollPercentage > 70) {
       this.paragraphScrollControl = 'paragraphScrolled';
     } else {
       this.paragraphScrollControl = 'notParagraphScrolled';
