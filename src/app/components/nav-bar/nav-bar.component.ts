89f93cb3d8ea17985cf4e38a3dd8fabeaf6a9a6b
Allan <allan.mendes0976@gmail.com>
2025-01-25 13:55:43 -0300

    feat: add responsive nav bar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css', './nav-bar-media.component.css'],
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  toggle: boolean = false;
+  windowWidth: boolean = false;
+
+  @HostListener('window:resize', [])
+  onResize(): void {
+    if (window.innerWidth > 840) {
+      this.windowWidth = true;
+    }
+  }
+
+  toggleMenu() {
+    if (window.innerWidth < 840) this.toggle = !this.toggle;
+  }
+}
